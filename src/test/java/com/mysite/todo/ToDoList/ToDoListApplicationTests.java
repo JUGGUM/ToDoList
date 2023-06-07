413b3b048d42cffacf1f9f8a75e06f07b17a2c2d
KKUMI <gayoung801@naver.com>
2023-06-07 13:41:27 +0900

    todoEntity test

--- src/test/java/com/mysite/todo/ToDoList/ToDoListApplicationTests.java
+++ src/test/java/com/mysite/todo/ToDoList/ToDoListApplicationTests.java
@@ -1,11 +1,27 @@
 package com.mysite.todo.ToDoList;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ToDoListApplicationTests {
 
+	@Autowired
+	private ToDoRepository todoRepository;
+
+	@Test
+	void testJpa(){
+		ToDoEntity todo1 = new ToDoEntity();
+		todo1.setContent("밥먹기");
+		todo1.setCompleted(Boolean.TRUE);
+		this.todoRepository.save(todo1);
+
+		ToDoEntity todo2 = new ToDoEntity();
+		todo2.setContent("스프링 공부하기");
+		todo2.setCompleted(Boolean.FALSE);
+		this.todoRepository.save(todo2);
+	}
 	@Test
 	void contextLoads() {
 	}
