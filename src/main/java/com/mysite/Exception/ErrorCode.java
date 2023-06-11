7cabb73d194bb2968a10073cd38f8df8a02311c9
KKUMI <gayoung801@naver.com>
2023-06-11 21:48:16 +0900

    에러코드 추가

--- src/main/java/com/mysite/Exception/ErrorCode.java
+++ src/main/java/com/mysite/Exception/ErrorCode.java
@@ -6,8 +6,9 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    NOT_FOUND(404, "COMMON-ERR-404", "페이지가 작동하지 않습니다."),
     BAD_REQUEST(400, "COMMON-ERR-400", "잘못된 경로입니다."),
+    INVALID_PERMISSION(401, "COMMMON-ERR-401", "권한이 존재하지 않습니다."),
+    NOT_FOUND(404, "COMMON-ERR-404", "페이지가 작동하지 않습니다."),
     INTER_SERVER_ERROR(500, "COMMON-ERR-500", "해당 ID가 존재하지 않습니다.");
 
     private int status;
